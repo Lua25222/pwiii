0f390f0c8a2eac0a04255d6e6a61f7af13bcd1da
20231350340 <20231350340@LTI-109-13>
2025-03-17 18:10:49 -0300

    Adicionar arquivos de projeto.

---
+++ pwiii/Controllers/loginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pwiii.Controllers
+{
+    public class loginController : Controller
+    {
+        public IActionResult Logar()
+        {
+            return View();
+        }
+    }
+}
